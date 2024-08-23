afe2921a00951ca2cfcf5e31a4652ce408df2b47
UltraPokemosha <sfygithub@gmail.com>
2024-08-23 14:41:25 +0300

    change APP title

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import react from "@vitejs/plugin-react-swc";
-import path from 'path';
+// import path from 'path'; 
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      "@c": path.resolve(__dirname, "./src/components"),
-    },
+    alias: [
+      {find: "@c", replacement: "/src/components"},
+      {find: "@", replacement: "/src"}
+    ]
+  },
+  build: {
+    rollupOptions: {
+      external: [""]
+    }
   },
   css: {
     preprocessorOptions: {
